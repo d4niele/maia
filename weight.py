1c60eb83141f14fccf89d65f1be11634683e2d19
Daniele <calupietru@gmail.com>
2019-05-22 17:32:32 +0200

    Update weight.py

--- weight.py
+++ weight.py
@@ -5,8 +5,8 @@ from hx711 import HX711
 driver = HX711(d_out=2, pd_sck=0)
 driver.channel=HX711.CHANNEL_A_64
 def kg():
-  m = -3.16916118e-04
-  n = -2.15724802e+01
-  p = m*driver.read()
-  return round(p,2)
+    m = -3.16916118e-04
+    n = -2.15724802e+01
+    p = m*driver.read()
+    return round(p,2)
   
