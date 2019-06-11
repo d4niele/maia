141624d33f2b367fbb41f14f3cd1bfb8b69b5d86
Daniele Rizzo <calupietru@gmail.com>
2019-06-11 20:32:26 +0200

    Update mymqtt.py

--- testing_mqtt/mymqtt.py
+++ testing_mqtt/mymqtt.py
@@ -2,11 +2,12 @@ from weight import kg
 from temp import get_data
 from umqtt.simple import MQTTClient
 import json,time
-client = MQTTClient("test1","calupietru.duckdns.org",port=1883,user="test1",password="test1")
-client.connect()
 def send_message():
+    client = MQTTClient("test1","calupietru.duckdns.org",port=1883,user="test1",password="test1")
+    client.connect()
     m = get_data()
     k = kg()
     message = {"espid":"Prato","timestamp":None,"temperatura":m[1],"umidità":m[0],"peso":str(k)}
     message["timestamp"]=time.time()
     client.publish("/maia/1",json.dumps(message))
+    client.disconnect()
