640efc83aa979306e842f52b93831fbbd71b64ed
Daniele Rizzo <calupietru@gmail.com>
2019-06-11 19:57:04 +0200

    Update mymqtt.py

--- testing_mqtt/mymqtt.py
+++ testing_mqtt/mymqtt.py
@@ -1,5 +1,5 @@
 from umqtt.simple import MQTTClient
-import json
+import json,time
 client = MQTTClient("test1","calupietru.duckdns.org",port=1883,user="test1",password="test1")
 client.connect()
 message = {"espid":"Prato","timestamp":None,"temperatura":23,"peso":70.1}
