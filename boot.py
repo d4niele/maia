8dc68c932b48b944ed236e747b23ddc7aea4bac1
Daniele <daniele.rizzo@spacespa.it>
2019-06-11 19:48:38 +0200

    inserito indirizzo fisso alla scheda

--- boot.py
+++ boot.py
@@ -1,13 +1,13 @@
 import esp
 esp.osdebug(None)
-
+import time
 import webrepl
 webrepl.start()
 
 import network
 n = network.WLAN(network.STA_IF)
 n.active(True)
-
+n.ifconfig(('192.168.1.x','255.255.255.0','192.168.1.1','192.168.1.1'))
 n.connect("ssid","password")
 
 
