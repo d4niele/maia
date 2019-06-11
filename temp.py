67ab0f3bb9b8356c9c107ed15edcd72b9af65935
Daniele Rizzo <calupietru@gmail.com>
2019-06-11 20:20:30 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,6 +1,6 @@
 import dht, machine
-d = dht.DHT22(machine.Pin(4))
 
 def get_data():
+    d = dht.DHT22(machine.Pin(4))
     d.measure() # funzione da richiamare prima di ogni nuova misurazione
     return [d.humidity(),d.temperature()]
