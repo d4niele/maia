bf9f38add0c473c288c0598bb45fba81893dd285
Daniele Rizzo <calupietru@gmail.com>
2019-06-12 08:09:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import network,time
 from mymqtt import send_message
+import gc
 TIME_INTERVAL = 5 
-time.sleep(TIME_INTERVAL)
 
+time.sleep(TIME_INTERVAL)
 n = network.WLAN(network.STA_IF)
 if n.isconnected():
     while True: #inserire la condizione per cui se un dato pin risulta alto
+	gc.collect()
         send_message()
         time.sleep(TIME_INTERVAL)
 	
